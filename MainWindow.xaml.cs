7a30b4a1bf2cf4d2b7567e3014cc466c1e9789c7
commander-hurricane <153162921+commander-hurricane@users.noreply.github.com>
2024-02-17 01:27:27 +0800

    登入界面

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab
+{
+    /// <summary>
+    ///MainWindow作为登录界面，登录成功跳转到其他界面
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        ///以上为登录界面的基础操作，往下接着写逻辑代码
+
+    }
+
+}
+
